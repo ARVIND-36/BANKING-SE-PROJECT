a2b79921439e244485d1f1e3cf59afea84be6bf0
ARVIND-36 <pnarvind06@gmail.com>
2026-02-16 21:59:39 +0530

    CHECK-ACTION-1

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -5,6 +5,14 @@ import logger from "../utils/logger.js";
 
 dotenv.config();
 
+if (!process.env.NEON_URL) {
+    logger.error("CRITICAL: NEON_URL environment variable is not defined");
+    // Don't crash immediately, but db calls will fail. 
+    // This allows index.js to run its check and log the error properly.
+    // Or at least throw a clear error.
+    throw new Error("NEON_URL environment variable is not defined");
+}
+
 const sql = neon(process.env.NEON_URL);
 const db = drizzle(sql);
 
